83acdab767e712cd769cfe53c226fafa38183aff
Pythonblyat <dansmolovik31@gmail.com>
2026-03-13 00:23:50 +0200

    gfdgdf

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Smolovyk Daniil");
+            int a;
+            int x, y;
+            Console.WriteLine("Введiть кількість хвилин:");      
+            a = int.Parse(Console.ReadLine());
+            x = a / 60;
+
+            y = a - (x * 60);
+            Console.WriteLine(x + "годин:" + y + "хвилин" );
+
+        }   
+    }
+}
